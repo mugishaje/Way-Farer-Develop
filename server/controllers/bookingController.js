1e35662d040a91db991977cf88db0e293b48fe27
mugishaje <mugishaje@gmail.com>
2019-08-08 19:39:45 +0200

    feat(deleteBooking): user can delete booking [Finishes #167791027]

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -15,12 +15,42 @@ const bookingController = {
                     data: database.bookings
                 });
             } else {
-                // console.log(database.bookings.find(booking => booking.user_email === decoded(req, res)));
+
                 return res.status(200).json({ status: 200, message: 'success', data: database.bookings.find(booking => booking.user_email === tokens.decoded(req, res).email) || "you have no booking" });
             }
         }
         return res.status(401).json({ status: 401, message: "You are unauthorized for this operation. Sign in first" });
     },
+
+    deleteBooking(req, res) {
+        const user = database.users.find(user => user.email === tokens.decoded(req, res).email);
+
+        if (user) {
+            const id = req.params.booking_id;
+            const booking = database.bookings.find(booking => booking.booking_id === parseInt(id, 10));
+
+            if (!booking) { return res.status(404).json({ status: 404, message: "the booking was not found" }) };
+
+            // if the booking exists check if it's id matches those the user has made
+            try {
+                if (booking.user_email === user.email) {
+
+                    const booking_index = database.bookings.indexOf(booking)
+                    database.bookings.splice(booking_index, 1)
+                    return res.status(200).json({
+                        status: 200,
+                        message: 'Booking has been deleted successfully',
+
+                    });
+                }
+                return res.status(401).json({ status: 401, message: "you can not delete another user's bookings" });
+            } catch (error) {
+                return res.status(500).json({ status: 500, message: 'something went wrong' })
+            }
+        }
+        return res.status(401).json({ status: 401, message: "You are unauthorized for this operation. Sign in first" });
+
+    }
 }
 
 export default bookingController
